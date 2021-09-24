703ae9794401bf7af16f8c7b1c85622ffc084bba
abubaker <mohammad_abubaker@yahoo.com>
2021-09-24 21:49:52 +0500

    Base setup

--- app/src/main/java/org/abubaker/projemanag/SplashActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/SplashActivity.kt
@@ -22,21 +22,21 @@ class SplashActivity : AppCompatActivity() {
         // Inflate Layout (XML)
         binding = DataBindingUtil.setContentView(this@SplashActivity, R.layout.activity_splash)
 
-        // TODO (Step 6: Add the full screen flags here.)
+        /**
+         * Full screen flags here.
+         */
+        // SDK 30 > This is used to hide the status bar and make the splash screen as a full screen activity.
+        // window.insetsController?.hide(WindowInsets.Type.statusBars())
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // This is used to hide the status bar and make the splash screen as a full screen activity.
-        // window.insetsController?.hide(WindowInsets.Type.statusBars())
 
-        // TODO (Step 7: Add the file in the custom assets file to the assets folder. And add the below line of code to apply it to the title TextView.)
-        // Steps for adding the assets folder are :
-        // Right click on the "app" package and GO TO ==> New ==> Folder ==> Assets Folder ==> Finish.
-
-
+        /**
+         * Custom Font
+         */
         // This is used to get the file from the assets folder and set it to the title textView.
         // Carbon Font Family: https://www.1001fonts.com/carbon-font.html
         val typeface: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
