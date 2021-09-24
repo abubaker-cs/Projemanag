7dfeb8dbba540c8ab8a0c430f3b256e2289c6e26
abubaker <mohammad_abubaker@yahoo.com>
2021-09-24 20:22:05 +0500

    Base setup

---
+++ app/src/main/java/org/abubaker/projemanag/MainActivity.kt
@@ -0,0 +1,21 @@
+package org.abubaker.projemanag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.projemanag.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Inflate Layout (XML)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+    }
+}
