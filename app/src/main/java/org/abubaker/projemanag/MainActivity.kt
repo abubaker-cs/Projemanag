9ed7faf0a0a97b0f5c8d2aa0dfcde6c0cbe9ec7d
abubaker <mohammad_abubaker@yahoo.com>
2021-09-24 20:34:48 +0500

    Base setup

--- app/src/main/java/org/abubaker/projemanag/MainActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
 
         // Inflate Layout (XML)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
--- app/src/main/java/org/abubaker/projemanag/SplashActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/SplashActivity.kt
@@ -2,10 +2,21 @@ package org.abubaker.projemanag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.projemanag.databinding.ActivityMainBinding
+import org.abubaker.projemanag.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        // setContentView(R.layout.activity_splash)
+
+        // Inflate Layout (XML)
+        binding = DataBindingUtil.setContentView(this@SplashActivity, R.layout.activity_splash)
+
     }
 }
