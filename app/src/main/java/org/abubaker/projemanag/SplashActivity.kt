ad60d9bfaa36d1098375b7506d28f3655a10cdd4
abubaker <mohammad_abubaker@yahoo.com>
2021-09-24 21:48:31 +0500

    Base setup

--- app/src/main/java/org/abubaker/projemanag/SplashActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/SplashActivity.kt
@@ -1,7 +1,10 @@
 package org.abubaker.projemanag
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import org.abubaker.projemanag.databinding.ActivityMainBinding
 import org.abubaker.projemanag.databinding.ActivitySplashBinding
@@ -12,11 +15,32 @@ class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_splash)
 
         // Inflate Layout (XML)
         binding = DataBindingUtil.setContentView(this@SplashActivity, R.layout.activity_splash)
+
+        // TODO (Step 6: Add the full screen flags here.)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        // window.insetsController?.hide(WindowInsets.Type.statusBars())
+
+        // TODO (Step 7: Add the file in the custom assets file to the assets folder. And add the below line of code to apply it to the title TextView.)
+        // Steps for adding the assets folder are :
+        // Right click on the "app" package and GO TO ==> New ==> Folder ==> Assets Folder ==> Finish.
+
+
+        // This is used to get the file from the assets folder and set it to the title textView.
+        // Carbon Font Family: https://www.1001fonts.com/carbon-font.html
+        val typeface: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding.tvAppName.typeface = typeface
 
     }
 }
