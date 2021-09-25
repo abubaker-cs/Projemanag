10741f8e233168f4bf38d428fb4639e5bcc0dc04
abubaker <mohammad_abubaker@yahoo.com>
2021-09-25 15:26:30 +0500

    Working...

--- app/src/main/java/org/abubaker/projemanag/SignUpActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/SignUpActivity.kt
@@ -2,10 +2,22 @@ package org.abubaker.projemanag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.projemanag.databinding.ActivityIntroBinding
+import org.abubaker.projemanag.databinding.ActivitySplashBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    // Binding Object
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        // Inflate Layout (XML)
+        binding = DataBindingUtil.setContentView(this@SignUpActivity, R.layout.activity_sign_up)
+
+
     }
+
 }
