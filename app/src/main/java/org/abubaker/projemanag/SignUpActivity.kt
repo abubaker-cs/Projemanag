d6a61bd8337d148c4029974a245d97d01f4ff3e2
abubaker <mohammad_abubaker@yahoo.com>
2021-09-25 15:39:54 +0500

    Working...

--- app/src/main/java/org/abubaker/projemanag/SignUpActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/SignUpActivity.kt
@@ -4,18 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import org.abubaker.projemanag.databinding.ActivityIntroBinding
+import org.abubaker.projemanag.databinding.ActivitySignUpBinding
 import org.abubaker.projemanag.databinding.ActivitySplashBinding
 
 class SignUpActivity : AppCompatActivity() {
 
     // Binding Object
-    private lateinit var binding: ActivitySplashBinding
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Inflate Layout (XML)
         binding = DataBindingUtil.setContentView(this@SignUpActivity, R.layout.activity_sign_up)
+
+        // Enabling Support for the Toolbar
+        setSupportActionBar(binding.toolbarSignUpActivity)
+
+        // Activating the Toolbar
+        val actionbar = supportActionBar
+
+        //
+        if (actionbar != null) {
+            actionbar!!.setDisplayHomeAsUpEnabled(true)
+            // actionbar!!.title = happyPlaceDetailModel.title
+        }
+
+    }
+
+    private fun setupActionBar() {
+
+
+
 
 
     }
