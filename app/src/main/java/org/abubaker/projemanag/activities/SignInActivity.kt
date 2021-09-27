5a29c9e77c0adad620a53be6e93104a510ccbc37
abubaker <mohammad_abubaker@yahoo.com>
2021-09-27 12:00:17 +0500

    Working on SignInActivity.kt

--- app/src/main/java/org/abubaker/projemanag/activities/SignInActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/activities/SignInActivity.kt
@@ -1,16 +1,23 @@
 package org.abubaker.projemanag.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import org.abubaker.projemanag.R
 import org.abubaker.projemanag.databinding.ActivitySignInBinding
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
 
     // Binding Object
     private lateinit var binding: ActivitySignInBinding
+
+    //
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +27,9 @@ class SignInActivity : AppCompatActivity() {
 
         // Inflate Layout (XML)
         binding = DataBindingUtil.setContentView(this@SignInActivity, R.layout.activity_sign_in)
+
+        //
+        auth = FirebaseAuth.getInstance()
 
         // SDK 30 > This is used to hide the status bar and make the splash screen as a full screen activity.
         // window.insetsController?.hide(WindowInsets.Type.statusBars())
@@ -55,4 +65,56 @@ class SignInActivity : AppCompatActivity() {
         }
 
     }
+
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+        val password: String = binding.etPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    /**
+     * A function to validate the entries of a user.
+     */
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
+    }
+
 }
