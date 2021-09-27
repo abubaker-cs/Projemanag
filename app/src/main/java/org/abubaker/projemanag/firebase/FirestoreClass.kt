9fc08ca4c8141b0a586835deeaae03c8107e7f05
abubaker <mohammad_abubaker@yahoo.com>
2021-09-27 16:31:05 +0500

    Working...

---
+++ app/src/main/java/org/abubaker/projemanag/firebase/FirestoreClass.kt
@@ -0,0 +1,55 @@
+package org.abubaker.projemanag.firebase
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import org.abubaker.projemanag.activities.SignUpActivity
+import org.abubaker.projemanag.models.User
+import org.abubaker.projemanag.utils.Constants
+
+/**
+ * A custom class where we will add the operation performed for the firestore database.
+ */
+class FirestoreClass {
+
+    // Create a instance of Firebase Firestore
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+    /**
+     * A function to make an entry of the registered user in the firestore database.
+     */
+    fun registerUser(activity: SignUpActivity, userInfo: User) {
+
+        mFireStore.collection(Constants.USERS)
+
+            // Document ID for users fields. Here the document it is the User ID.
+            .document(getCurrentUserID())
+
+            // Here the userInfo are Field and the SetOption is set to merge. It is for if we wants to merge
+            .set(userInfo, SetOptions.merge())
+
+            .addOnSuccessListener {
+
+                // Here call a function of base activity for transferring the result to it.
+                activity.userRegisteredSuccess()
+
+            }
+            .addOnFailureListener { e ->
+
+                Log.e(
+                    activity.javaClass.simpleName,
+                    "Error writing document",
+                    e
+                )
+
+            }
+    }
+
+    /**
+     * A function for getting the user id of current logged user.
+     */
+    fun getCurrentUserID(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+}
---
+++ app/src/main/java/org/abubaker/projemanag/utils/Constants.kt
@@ -0,0 +1,12 @@
+package org.abubaker.projemanag.utils
+
+/**
+ * a constants object and make an users collection constant value
+ */
+object Constants {
+
+    // Firebase Constants
+    // This  is used for the collection name for USERS.
+    const val USERS: String = "users"
+
+}
