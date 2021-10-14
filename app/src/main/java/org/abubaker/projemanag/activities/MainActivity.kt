7fd136ea0f73fc5641194bc40053f0be23412678
abubaker <mohammad_abubaker@yahoo.com>
2021-10-15 01:53:44 +0500

    Implemented function to also close Drawer when the BACK button will be pressed

--- app/src/main/java/org/abubaker/projemanag/activities/MainActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/activities/MainActivity.kt
@@ -1,13 +1,18 @@
 package org.abubaker.projemanag.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import org.abubaker.projemanag.R
 import org.abubaker.projemanag.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     // Binding Object
     private lateinit var binding: ActivityMainBinding
@@ -72,10 +77,41 @@ class MainActivity : BaseActivity() {
             // We will close the drawable, since it was already opened
             binding.drawerLayout.closeDrawer(GravityCompat.START)
 
+
         } else {
 
             // A double back press function is added in Base Activity.
             doubleBackToExit()
         }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+
+        when (menuItem.itemId) {
+
+            // My Profile
+            R.id.nav_my_profile -> {
+
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+
+            // Sign Out
+            R.id.nav_sign_out -> {
+
+                // Here sign outs the user from firebase in this device.
+                FirebaseAuth.getInstance().signOut()
+
+                // Send the user to the intro screen of the application.
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
     }
 }
