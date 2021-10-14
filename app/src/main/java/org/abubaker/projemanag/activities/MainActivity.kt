0c0fb9764180d83c12fe1a587eab0b9a3ee3d8c6
abubaker <mohammad_abubaker@yahoo.com>
2021-10-14 22:56:42 +0500

    Working...

--- app/src/main/java/org/abubaker/projemanag/activities/MainActivity.kt
+++ app/src/main/java/org/abubaker/projemanag/activities/MainActivity.kt
@@ -19,5 +19,50 @@ class MainActivity : AppCompatActivity() {
         // Inflate Layout (XML)
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
 
+        // Setup ActionBar
+        setupActionBar()
+
+
     }
+
+    /**
+     * A function to setup action bar
+     */
+    private fun setupActionBar() {
+
+        // setSupportActionBar(toolbar_main_activity)
+
+        // toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+
+        // Enabling Support for the Toolbar
+        setSupportActionBar(binding.toolbarMainActivity)
+
+        // Activating the Toolbar
+        val actionbar = supportActionBar
+
+        //
+        if (actionbar != null) {
+            // actionbar!!.setDisplayHomeAsUpEnabled(true)
+            // actionbar!!.title = happyPlaceDetailModel.title
+        }
+
+        // Click event for navigation in the action bar and call the toggleDrawer function
+        // toolbar_main_activity.setNavigationOnClickListener {
+        //     toggleDrawer()
+        // }
+    }
+
+    /**
+     * A function for opening and closing the Navigation Drawer.
+     */
+    private fun toggleDrawer() {
+
+//        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+//            drawer_layout.closeDrawer(GravityCompat.START)
+//        } else {
+//            drawer_layout.openDrawer(GravityCompat.START)
+//        }
+    }
+    // END
 }
