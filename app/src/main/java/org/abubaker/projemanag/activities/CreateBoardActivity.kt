c9861f014d0b36ea0f1c3e7d53a5b961e4d05e54
abubaker <mohammad_abubaker@yahoo.com>
2021-10-24 15:50:20 +0500

    Added new assets in the drawable and working on creating markup for the CreateBoardActivity

---
+++ app/src/main/java/org/abubaker/projemanag/activities/CreateBoardActivity.kt
@@ -0,0 +1,11 @@
+package org.abubaker.projemanag.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_board)
+    }
+}
