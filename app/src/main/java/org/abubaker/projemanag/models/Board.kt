60f157c27379f25057815fbcac8baa9e9e6dbfe7
abubaker <mohammad_abubaker@yahoo.com>
2021-10-25 00:23:58 +0500

    Working on the Board.kt

---
+++ app/src/main/java/org/abubaker/projemanag/models/Board.kt
@@ -0,0 +1,11 @@
+package org.abubaker.projemanag.models
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Board(
+    val name: String = "",
+    val image: String = "",
+    val createdBy: String = "",
+    val assignedTo: ArrayList<String> = ArrayList()
+)
